6eeeaa927550437f230adbc7346945d4aea80810
inku0 <ingvar@saarend.com>
2023-10-22 18:59:12 +0300

    working

---
+++ downloader.py
@@ -0,0 +1,31 @@
+from seleniumwire import webdriver
+from selenium.webdriver.firefox.options import Options
+import subprocess
+
+
+def get_jupiter(jupiter_url):
+    # Create a new instance of the Firefox driver in headless mode
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(
+        options=options
+    )
+
+    # Go to the Jupiter link
+    driver.get(jupiter_url)
+    print(f"URL = {jupiter_url}")
+
+    # get the master m3u8
+    [master] = [item.url for item in driver.requests if item.response and "master" in item.url]
+    print(f"master = {master}")
+    driver.quit()
+
+    subprocess.run(
+        ["yt-dlp",
+         "-f", "bv*+mergeall[vcodec=none]",
+         "--audio-multistreams",
+         "--convert-subs", "srt",
+         "--embed-subs",
+         "--external-downloader", "aria2c",
+         "--external-downloader-args", "-c -j 8 -x 8 -s 8 -k 2M",
+         master])
---
+++ main.py
@@ -0,0 +1,4 @@
+from downloader import get_jupiter
+
+if __name__ == "__main__":
+    get_jupiter("https://jupiter.err.ee/1608445664/tove-janssoni-raakimata-lugu")
